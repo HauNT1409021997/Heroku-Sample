e31046541d1a63a6a87cf66284cd2f8589b8c803
Nguyễn Trọng Hậu <akitosama12546@gmail.com>
2022-09-08 00:09:56 +0700

    20220908 2 commits

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:123@localhost:5432/TODO"
+export DATABASE_URL="postgres://gposjlbtsfujbc:dbd3e27825659583b14fc565bcdd5e664319b5ab4c00848ee05ae47ef16a0231@ec2-44-207-253-50.compute-1.amazonaws.com:5432/deoa0utbc7q73v"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
